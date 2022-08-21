085db18f627174102a2be9afb0c86e58eccbaa14
khoatran <ktran18@huskers.unl.edu>
2022-08-21 23:21:26 +0000

    change the print statement

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 #include<stdio.h>
 
 int main (int argc, char **argv) {
-    printf("Hello my name is Khoa\n");
-    printf("My major is Computer Science\n");
+    printf("Hello my name is Khoa!\n");
+    printf("My major is Computer Science and Mathematics minor\n");
 
     return 0;
 }
