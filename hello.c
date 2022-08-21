d35c8b18bff610c8965721d405d5168fefe55aaa
khoatran <ktran18@huskers.unl.edu>
2022-08-21 23:11:27 +0000

    my initial commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main (int argc, char **argv) {
+    printf("Hello my name is Khoa\n");
+    printf("My major is Computer Science\n");
+
+    return 0;
+}
